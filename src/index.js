4a46bed6709d58cde2329811eed31bd06a594b16
Ezzdin Atef <ezzdin1125@gmail.com>
2021-08-11 05:39:29 +0200

    fix issue

--- src/index.js
+++ src/index.js
@@ -1,42 +1,38 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const async = require("async");
-const readXlsxFile = require('read-excel-file/node');
+const readXlsxFile = require("read-excel-file/node");
 
 const names = [];
 
 function delay(time) {
-  return new Promise(function(resolve) { 
-      setTimeout(resolve, time)
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
   });
 }
 
-readXlsxFile('./Contacts.xlsx').then((rows) => {
-  rows.forEach(el => names.push(el[0]));
+readXlsxFile("./src/Contacts.xlsx").then((rows) => {
+  rows.forEach((el) => names.push(String(el[2])));
   (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto("https://web.whatsapp.com/");
-    const message = "Last Test yaraaab  😂‍";
-  
-    async.eachSeries(names, async name => {
+    const message = "testing message";
+
+    async.eachSeries(names, async (name) => {
       await page.waitForSelector("#side .selectable-text");
       const search = await page.$("#side .selectable-text");
-      await page.$eval("#side .selectable-text", el => el.value = "");
+      await page.$eval("#side .selectable-text", (el) => (el.value = ""));
       await search.type(name);
-  
-      await page.waitForSelector("span [title='"+ name +"']");
-      const person = await page.$("span [title='"+ name +"']");
+
+      await page.waitForSelector("span [title='" + name + "']");
+      const person = await page.$("span [title='" + name + "']");
       await person.click();
-  
+
       await page.waitForSelector("#main .selectable-text");
       const input = await page.$("#main .selectable-text");
       await input.type(message);
       await page.keyboard.press("Enter");
       await delay(5000);
     });
-  
-    
-  
   })();
 });
-
